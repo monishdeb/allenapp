ff8f979a7868aaf01a19adf1fab190a5a245976e
Monish Deb <monish.deb@jmaconsulting.biz>
2026-04-14 14:29:59 +0530

    Update AllenAppLifeCycleDisplay.dart with new content

---
+++ AllenAppLifeCycleDisplay.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../screens/login.dart';
+import 'query.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/scheduler.dart';
+import 'auth.dart';
+
+class AllenAppLifeCycleDisplay extends StatefulWidget {
+  const AllenAppLifeCycleDisplay({super.key});
+
+  @override
+  State<AllenAppLifeCycleDisplay> createState() => _AllenAppLifeCycleDisplayState();
+}
+
+class _AllenAppLifeCycleDisplayState extends State<AllenAppLifeCycleDisplay> {
+  late final AppLifecycleListener _listener;
+  late AppLifecycleState? _state;
+  int now = DateTime.now().millisecondsSinceEpoch;
+
+  @override
+  void initState() {
+    super.initState();
+    _state = SchedulerBinding.instance.lifecycleState;
+    _listener = AppLifecycleListener(
+      onResume: () => {
+        checkToNavigateToHome()
+      },
+      onInactive: () => {
+        checkToNavigateToHome()
+      },
+      onRestart: () => {
+        checkToNavigateToHome()
+      }
+    );
+  }
+
+  void checkToNavigateToHome() async {
+    int? last_pin_request = await getLastPinCodeRequest();
+    
+    // Only navigate if the pin code request timeout has expired
+    // This prevents pushing the login route when returning from background
+    if (last_pin_request != null && last_pin_request <= now) {
+      if (!kIsWeb && (Platform.isAndroid || Platform.isLinux)) {
+        navigatorKey.currentState?.pushReplacementNamed('/', arguments: {'forceLogin': true});
+      }
+      else if (kIsWeb) {
+        navigatorKey.currentState?.pushReplacementNamed('/', arguments: {'forceLogin': true});
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+          title: 'Allen App',
+          theme: ThemeData(
+            primaryColor: Colors.white,
+            scaffoldBackgroundColor: Colors.white,
+
+            // GLOBAL APP BAR SETTINGS
+            appBarTheme: AppBarTheme(
+              backgroundColor: Color.fromRGBO(213, 31, 39, 1),
+              centerTitle: true,
+              iconTheme: const IconThemeData(
+                color: Colors.white,
+              ),
+              titleTextStyle: const TextStyle(
+                fontFamily: 'helvetica,sans-serif',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            snackBarTheme: SnackBarThemeData(
+              backgroundColor: Color.fromRGBO(213, 31, 39, 1),
+            ),
+          ),
+          navigatorKey: navigatorKey,
+          routes: {
+            '/': (context) => LoginPage(title: 'Allen app', authenticated: false),
+          }
+      ),
+    );
+  }
+}
